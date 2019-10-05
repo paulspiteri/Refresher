70a66e0137b26d661afe70c29eff9997563b9f9b
Paul Spiteri <paulspiteri@hotmail.com>
2019-10-05 16:18:46 -0400

    upgrade to newer win10 version and add connection timeout

--- Refresher10/MainPage.xaml.cs
+++ Refresher10/MainPage.xaml.cs
@@ -11,15 +11,23 @@ namespace Refresher10
     {
         private int _reloadCounter;
 
+        private const int MAX_TIMEOUT_MS = 2000;
+        private readonly DispatcherTimer _countdownTimer = new DispatcherTimer()
+        {
+            Interval = new TimeSpan(0, 0, 0, 0, MAX_TIMEOUT_MS)
+        };
+
         public MainPage()
         {
             InitializeComponent();
-            NavButton.Click += NavButton_Click;
             GoButton.Click += GoButton_OnClick;
             NewWindowButton.Click += NewWindowButton_Click;
 
             UrlTextBox.Text = "";
-            KeywordTextBox.Text = "holding";
+            KeywordTextBox.Text = "free space";
+
+            WebBrowserControl.DOMContentLoaded += WebBrowserControl_DOMContentLoaded;
+            _countdownTimer.Tick += OnTimeoutElapsed;
         }
 
         private async void NewWindowButton_Click(object sender, RoutedEventArgs e)
@@ -38,31 +46,30 @@ namespace Refresher10
             await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newAppView.Id);
         }
 
-        private void NavButton_Click(object sender, RoutedEventArgs e)
-        {
-            WebBrowserControl.Source = new Uri(UrlTextBox.Text);
-        }
-
         private void GoButton_OnClick(object sender, RoutedEventArgs e)
         {
             GoButton.IsEnabled = false;
-            WebBrowserControl.DOMContentLoaded += WebBrowserControl_DOMContentLoaded;
-            if (WebBrowserControl.Source != null)
-            {
-                Reload();
-            }
+            Reload();
+        }
+
+        private void OnTimeoutElapsed(object sender, object e)
+        {
+            WebBrowserControl.Stop();
+            _countdownTimer.Stop();
+
+            Reload();
         }
 
         private async void WebBrowserControl_DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
         {
+            _countdownTimer.Stop();
+
             var htmlText = await sender.InvokeScriptAsync("eval", new[] { "document.documentElement.outerHTML;" });
             if (string.IsNullOrWhiteSpace(htmlText))
             {
                 Reload();
-                return;
             }
-
-            if (htmlText.IndexOf(KeywordTextBox.Text, StringComparison.OrdinalIgnoreCase) >= 0)
+            else if (htmlText.IndexOf(KeywordTextBox.Text, StringComparison.OrdinalIgnoreCase) >= 0)
             {
                 Reload();
             }
@@ -74,17 +81,18 @@ namespace Refresher10
 
         private void Stop()
         {
-            WebBrowserControl.DOMContentLoaded -= WebBrowserControl_DOMContentLoaded;
             GoButton.IsEnabled = true;
         }
 
         private void Reload()
         {
+            _countdownTimer.Start();
+
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
                 () =>
                 {
-                    WebBrowserControl.Refresh();
+                    WebBrowserControl.Navigate(new Uri(UrlTextBox.Text));
                     _reloadCounter++;
                     RefreshCountTextBlock.Text = _reloadCounter.ToString();
                 });
